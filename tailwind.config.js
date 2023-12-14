40e6cef30e5a49d3ca368657ee8e1142286e27a0
Lavda <kaustubhwahane55@gmail.com>
2023-12-14 23:17:39 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/index.html"],
+  theme: {
+    extend: {
+      fontfamily:{
+        fontfamily: 'Gloria Hallelujah',
+        fontfamily: 'Luckiest Guy',
+    }},
+  },
+  plugins: [],
+}
+
